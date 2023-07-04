1de30d9b7737a4283d7ec20306d9a180e75cc9a1
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-07-04 17:09:54 -0500

    Interfaz Principal terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unnecessary_const
-
 import 'package:flutter/material.dart';
 import 'package:pet_rescue_flutter/login.dart';
 
@@ -15,16 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: const MaterialColor(0xFF7689DE, <int, Color>{
-        50: const Color(0xFFE8EAF6),
-        100: const Color(0xFFC5CAE9),
-        200: const Color(0xFF9FA8DA),
-        300: const Color(0xFF7986CB),
-        400: const Color(0xFF5C6BC0),
-        500: const Color(0xFF3F51B5),
-        600: const Color(0xFF3949AB),
-        700: const Color(0xFF303F9F),
-        800: const Color(0xFF283593),
-        900: const Color(0xFF1A237E),
+        50: Color(0xFFE8EAF6),
+        100: Color(0xFFC5CAE9),
+        200: Color(0xFF9FA8DA),
+        300: Color(0xFF7986CB),
+        400: Color(0xFF5C6BC0),
+        500: Color(0xFF3F51B5),
+        600: Color(0xFF3949AB),
+        700: Color(0xFF303F9F),
+        800: Color(0xFF283593),
+        900: Color(0xFF1A237E),
       })),
       home: const LoginScreen(),
     );
